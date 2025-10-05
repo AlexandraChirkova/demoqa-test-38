d7e98e0d7cf0c1c5d18f38a201bdbb7ed199a1f9
alexandra <alexandra.chirkova1208@yandex.kz>
2025-10-05 21:46:22 +0500

    add some new action

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FirstTest {
@@ -24,10 +24,17 @@ public class FirstTest {
         $("#userEmail").setValue("alexandra,chirkova88@gmail.com");
         $("label[for='gender-radio-2']").click();
         $("#userNumber").setValue("87051813123");
-        $("#dateOfBirthInput").click();
-        $("").setValue("").pressEnter();
+        $("#dateOfBirthInput").scrollTo().click();
+        $(".react-datepicker__month-select").selectOption("August");
+        $(".react-datepicker__year-select").selectOption("1988");
+        $$(".react-datepicker__day")
+                .findBy(text("12"))
+                .click();
+        $("#dateOfBirthInput").shouldHave(value("12 Aug 1988"));
+        $("#subjectsInput").scrollTo().setValue("Maths").pressEnter();
+        $(".subjects-auto-complete__multi-value__label")
+                .shouldHave(text("Maths"));
         $("label[for='hobbies-checkbox-2']").click();
-
         $("#currentAddress").setValue("Almaty");
 
 
