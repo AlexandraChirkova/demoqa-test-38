25cf7e98651ece1c7b6bf80b0135bde64d0f537c
makeicheva_21303 <alexandra.chirkova@kaspi.kz>
2025-10-06 09:52:39 +0500

    add first test and check result

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -2,8 +2,6 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.time.Duration;
-
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -15,13 +13,12 @@ public class FirstTest {
         Configuration.baseUrl="https://demoqa.com";
     }
 
-
     @Test
     void applicationFormTest() {
         open("/automation-practice-form");
         $("#firstName").setValue("Alexandra");
         $("#lastName").setValue("Chirkova");
-        $("#userEmail").setValue("alexandra,chirkova88@gmail.com");
+        $("#userEmail").setValue("alexandra.chirkova88@gmail.com");
         $("label[for='gender-radio-2']").click();
         $("#userNumber").setValue("87051813123");
         $("#dateOfBirthInput").scrollTo().click();
@@ -35,8 +32,26 @@ public class FirstTest {
         $(".subjects-auto-complete__multi-value__label")
                 .shouldHave(text("Maths"));
         $("label[for='hobbies-checkbox-2']").click();
+        $("#uploadPicture").uploadFromClasspath("images/img1.jpeg");
         $("#currentAddress").setValue("Almaty");
+        $("#state").click();
+        $x("//div[contains(@id,'react-select') and text()='NCR']").click();
+        $("#city").click();
+        $x("//div[contains(@id,'react-select') and text()='Delhi']").click();
+        $("#submit").click();
 
-
+        $(".modal-content").shouldBe(visible);
+        $(".modal-content").$(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $x("//td[text()='Student Name']/following-sibling::td").shouldHave(text("Alexandra Chirkova"));
+        $x("//td[text()='Student Email']/following-sibling::td").shouldHave(text("alexandra.chirkova88@gmail.com"));
+        $x("//td[text()='Gender']/following-sibling::td").shouldHave(text("Female"));
+        $x("//td[text()='Mobile']/following-sibling::td").shouldHave(text("8705181312"));
+        $x("//td[text()='Date of Birth']/following-sibling::td").shouldHave(text("12 August,1988"));
+        $x("//td[text()='Subjects']/following-sibling::td").shouldHave(text("Maths"));
+        $x("//td[text()='Hobbies']/following-sibling::td").shouldHave(text("Reading"));
+        $x("//td[text()='Picture']/following-sibling::td").shouldHave(text("img1.jpeg"));
+        $x("//td[text()='Address']/following-sibling::td").shouldHave(text("Almaty"));
+        $x("//td[text()='State and City']/following-sibling::td").shouldHave(text("NCR Delhi"));
     }
+
 }
